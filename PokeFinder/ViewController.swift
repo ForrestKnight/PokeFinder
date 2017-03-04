756f62c5194b19b917b1593c77c7beaafbc0aa2f
ForrestKnight <fknig001@odu.edu>
2017-03-04 11:22:30 -0500

    Initial Commit

---
+++ PokeFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeFinder
+//
+//  Created by Forrest Knight on 3/4/17.
+//  Copyright © 2017 Forrest Knight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
