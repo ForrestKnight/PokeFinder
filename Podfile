647cc1a0e8fb05bd105222deeeab9234b500431e
ForrestKnight <fknig001@odu.edu>
2017-03-22 00:18:21 -0400

    Added Firebase & GeoFire

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PokeFinder' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PokeFinder
+  pod 'Firebase/Core'
+  pod 'GeoFire', :git => 'https://github.com/firebase/geofire-objc.git'
+end
--- PokeFinder/ViewController.swift
+++ PokeFinder/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  PokeFinder
@@ -7,19 +8,184 @@
 //
 
 import UIKit
+import MapKit
+import GeoFire
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    var geoFire: GeoFire!
+    var geoFireRef: FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow //The map follows user's location
+        
+        geoFireRef = FIRDatabase.database().reference()
+        geoFire = GeoFire(firebaseRef: geoFireRef)
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        //Only find location when app is in use
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            
+            mapView.showsUserLocation = true //Shows user location
+        } else {
+            
+            locationManager.requestWhenInUseAuthorization() //Prompt "can I access your location"
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            
+            mapView.showsUserLocation = true
+            
+        }
+        
+    }
+    
+    //Determining the size of region shown in the app upon launch
+    func centerMapOnLocation(location: CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+    }
+    
+    //Tells map to center
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        if let loc = userLocation.location {
+            
+            //Makes sure the map doesn't keep recentering while someone is walking and trying to pan to find pokemon
+            if !mapHasCenteredOnce {
+                centerMapOnLocation(location: loc)
+                mapHasCenteredOnce = true
+            }
+        }
+    }
+    
+    //Replace blue dot with Ash for user location. viewFor annotation is where you'll do all customization for setting down pins in mapViews.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let annoIdentifier = "Pokemon"
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "Ash")
+           
+          //Reuse annotations if needed
+        } else if let deqAnno = mapView.dequeueReusableAnnotationView(withIdentifier: annoIdentifier) {
+            
+            annotationView = deqAnno
+            annotationView?.annotation = annotation
+          
+          //Create a new annotation if we can't reuse one
+        } else {
+            
+            let av = MKAnnotationView(annotation: annotation, reuseIdentifier: annoIdentifier)
+            
+            av.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView = av
+        }
+        
+        
+        if let annotationView = annotationView, let anno = annotation as? PokeAnnotation {
+            
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "\(anno.pokemonNumber)")
+            
+            //This make the image a button.
+            let btn = UIButton()
+            btn.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            btn.setImage(UIImage(named: "map"), for: .normal)
+            annotationView.rightCalloutAccessoryView = btn
+        }
+        
+        
+        return annotationView
+    }
+    
+    func createSighting(forLocation location: CLLocation, withPokemon pokeId: Int) {
+        
+        geoFire.setLocation(location, forKey: "\(pokeId)")
+    }
+    
+    func showSightingsOnMap(location: CLLocation) {
+        let circleQuery = geoFire!.query(at: location, withRadius: 2.5)
+        
+        //The _ is if you don't want a result to a variable.
+        _ = circleQuery?.observe(GFEventType.keyEntered, with: { (key, location) in
+            
+            //This gets called for amount of pokemon (151 times).
+            //We are renaming the former key variable from the line above to the latter key variable in the line below so if we reference it in the local scope, it won't access it in the outer scope.
+            if let key = key, let location = location {
+                //Passing in the pokeNumber and location coordinates so when it shows on the map, it drops the pokemon image.
+                let anno = PokeAnnotation(coordinate: location.coordinate, pokemonNumber: Int(key)!)
+                self.mapView.addAnnotation(anno)
+            }
+        })
+    }
+    
+    //Updates the map as the user pans
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        //Grabbing center location for wherever the user just scrolled
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        
+        showSightingsOnMap(location: loc)
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        //Pulls up Apple maps to take you to selected location
+        if let anno = view.annotation as? PokeAnnotation {
+    
+            let place = MKPlacemark(coordinate: anno.coordinate)
+            let destination = MKMapItem(placemark: place)
+            destination.name = "Pokemon Sighting"
+            let regionDistance: CLLocationDistance = 1000
+            let regionSpan = MKCoordinateRegionMakeWithDistance(anno.coordinate, regionDistance, regionDistance)
+            
+            //Configuring map before opening
+            let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span), MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving] as [String : Any]
+            
+            //Open up maps with the presets listed above
+            MKMapItem.openMaps(with: [destination], launchOptions: options)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func spotRandomPokemon(_ sender: AnyObject) {
+        
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        let rand = arc4random_uniform(151) + 1
+        createSighting(forLocation: loc, withPokemon: Int(rand))
+        
     }
-
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
